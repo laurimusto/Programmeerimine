562cf3575541af93a451890815774b8b1b39ee64
laurimusto <laurimusto@gmail.com>
2013-10-31 13:02:51 +0200

    Delete 5.py

--- 5.py
+++
@@ -1,9 +0,0 @@
-kes = raw_input("Kes sai valimistel k6ige v2hem h22li? ")
-if kes == "algis":
-	oige = raw_input(" 6ige! Mitu h22lt ta sai? ")
-	if oige == "2":
-		print "Tubli oled, eluga kursis!"
-	else:
-		print "peaaegu!"
-else:
-	print "Kuidas sa siis ei tea! Muidugi oli see Algis Suurkivi!"
