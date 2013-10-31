3c330dd8fb2fc943a076e85af0b61a66680f118f
opilane <opilane@PC215-10.(none)>
2013-10-31 13:01:33 +0200

    jou

--- 1.py
+++ 1.py
@@ -1,5 +1,7 @@
-kes = raw_input('Kes on Sandrile v6lgu? ')
-if kes == "Peeter":
-	print "Jah, teil on 6igus. Peeter on Sandrile v6lgu."
-else:
-	print "Sa ikka ei tea!"
+jou = raw_input('Palun sisesta oma nimi: ');
+
+print jou.upper();
+print jou.capitalize();
+print jou.lower();
+print jou.swapcase();
+print jou.strip('Man')
