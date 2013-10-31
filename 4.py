58ca9e8389f327072507f2d1de6b8fa9b8e8c604
laurimusto <laurimusto@gmail.com>
2013-10-31 13:02:37 +0200

    Delete 4.py

--- 4.py
+++
@@ -1,6 +0,0 @@
-asd = raw_input("Sisesta oma eesnimi: ")
-fgh = raw_input("Sisesta oma perenimi: ")
-print "Sinu nimi on: " + asd + " " + fgh
-qwe = raw_input("Sisesta oma vanus: ")
-print " Sinu nimi on: " + asd + fgh + " ja sinu vanus on " + qwe + " aastat"
-
