329ffa31cddec14b98a0f142904008d0e767eeaa
opilane <opilane@PC215-10.(none)>
2013-11-06 10:57:36 +0200

    uudiskiri

---
+++ uudiskiri.py
@@ -0,0 +1,22 @@
+p2is = "P2is"
+jalus = "jalus"
+tyhi = ""
+pealkiri1 = "Algis Suurkivi sai vallavolikogu valimistel ainult 2 h22lt! "
+pealkiri2 = "kahjuks sellest ei piisanud ning Algis ei p22senud vallavolikokku"
+loik1 = "Kanepi suur lootus Algis Suurkivi sai valimistel k6igest 2 h22lt"
+loik2 = "Selle teoga vedas Algis tervet VKHK AAp-11't alt ning ta ei v22ri enam reformierakonnas olemist."
+coun = sum(c.isalpha() for c in loik1) 
+
+print p2is.center(55, "=")
+print pealkiri1.center(55, " ")
+print tyhi.center(55, " ")
+print loik1.rjust(50, " ")
+        
+
+print tyhi.center(55, " ")
+print pealkiri2.center(55, " ")
+print tyhi.center(55, " ")
+print loik2.rjust(55, " ")
+print tyhi.center(55, " ")
+print jalus.center(55, "=")
+
