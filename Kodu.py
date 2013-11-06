c60f085c0597ec2f14923df2b8fbc66f93a4326d
laurimusto <laurimusto@gmail.com>
2013-11-06 09:59:41 +0200

    Update Kodu.py

--- Kodu.py
+++ Kodu.py
@@ -5,11 +5,11 @@ email = raw_input("Sisesta oma email: ")
 tel = raw_input("Sisesta oma telefoni number: ")
 tekst = "Visiitkaart"
 tyhi = ""
-print "|", tekst.center(80, "="), '|' #tyhi rida
-print "|", tyhi.center(80, " "), '|'
-print "|", nimi.center(80), "|"
-print "|", amet.center(80), "|"    
-print "|", email.center(80), "|"
-print "|", tel.center(80), "|"
-print "|", tyhi.center(80, " "), '|'#tyhi rida
-print "|", tyhi.center(80, "="), '| \n'
+print "|", tekst.center(70, "="), '|' #tyhi rida
+print "|", tyhi.center(70, " "), '|'
+print "|", nimi.center(70), "|"
+print "|", amet.center(70), "|"    
+print "|", email.center(70), "|"
+print "|", tel.center(70), "|"
+print "|", tyhi.center(70, " "), '|'#tyhi rida
+print "|", tyhi.center(70, "="), '| \n'
