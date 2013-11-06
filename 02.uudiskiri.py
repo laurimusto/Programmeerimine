708f5321c35370568161fbe60b2109616e4db1d8
laurimusto <laurimusto@gmail.com>
2013-11-06 10:59:49 +0200

    Update 02.uudiskiri.py

--- 02.uudiskiri.py
+++ 02.uudiskiri.py
@@ -5,7 +5,6 @@ pealkiri1 = "Algis Suurkivi sai vallavolikogu valimistel ainult 2 h22lt! "
 pealkiri2 = "kahjuks sellest ei piisanud ning Algis ei p22senud vallavolikokku"
 loik1 = "Kanepi suur lootus Algis Suurkivi sai valimistel k6igest 2 h22lt"
 loik2 = "Selle teoga vedas Algis tervet VKHK AAp-11't alt ning ta ei v22ri enam reformierakonnas olemist."
-coun = sum(c.isalpha() for c in loik1) 
 
 print p2is.center(55, "=")
 print pealkiri1.center(55, " ")
