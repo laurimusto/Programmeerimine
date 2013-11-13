2135bb658db4020a340c5c597460cfa7760101f4
opilane <opilane@PC215-10.(none)>
2013-11-13 10:52:22 +0200

    update

--- 04.matemaatika.py
+++ 04.matemaatika.py
@@ -6,9 +6,6 @@ ruumala = z * y * x
 print ruumala
 
 
-x = 100/math.sin(50 * math.pi/18)
-print 'vastus on: x = ',round(x,2),'cm';
-
 
 nummi = raw_input("Kirjutage number palun ")
 numps = float(num)
