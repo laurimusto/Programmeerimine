1ae43ee279115e0595cdb909811cff5a63ea1de4
Lauri Musto <laurimusto@gmail.com>
2013-11-13 10:50:07 +0200

    Update 1.py
    rename

--- 1.py
+++ 1.py
@@ -1,7 +1,18 @@
-jou = raw_input('Palun sisesta oma nimi: ');
+print "ruum"
+x = 12
+y = 13
+z = 14
+ruumala = z * y * x
+print ruumala
 
-print jou.upper();
-print jou.capitalize();
-print jou.lower();
-print jou.swapcase();
-print jou.strip('Man')
+
+x = 100/math.sin(50 * math.pi/18)
+print 'vastus on: x = ',round(x,2),'cm';
+
+
+nummi = raw_input("Kirjutage number palun ")
+numps = float(num)
+print "Tangens: ",math.tan(numps * math.pi/180)
+print "Cos siinus: ",math.cos(numps * math.pi/180)
+print "Siinus : ",math.sin(numps * math.pi/180)
+print "jou"
